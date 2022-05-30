4c84f97e44e1da76ddda36262a49f564d18a3ebc
Fraknl <frank.torres0410@ucuenca.edu.ec>
2022-05-29 19:05:30 -0500

    usuarios2

--- src/keysHeroku.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database:{
-        host: 'us-cdbr-east-05.cleardb.net',
-        user: 'b3fe73cc85de04',
-        password: '1ff9ec2f',
-        database: 'heroku_3d46f895bda9072'
-    }
-}
