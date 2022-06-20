1eb59ef2a3bf1b6ff758c926a59ff9c80d24feef
Fraknl <frank.torres0410@ucuenca.edu.ec>
2022-06-20 17:34:59 -0500

    buffer

--- src/funciones.js
+++ src/funciones.js
@@ -35,13 +35,13 @@ function parseMessage (buffer) {
  
     const data = Buffer.alloc(payloadLength);
     if (isMasked) {
-      for (let i = 0, j = 0; i < payloadLength && i<8; ++i, j = i % 4) {
+      for (let i = 0, j = 0; i < payloadLength && i<Buffer.byteLength(buffer); ++i, j = i % 4) {
         const shift = j == 3 ? 0 : (3 - j) << 3; 
         const mask = (shift == 0 ? maskingKey : (maskingKey >>> shift)) & 0xFF;
+        const source = '';
         try {
-          const source = buffer.readUInt8(currentOffset++);
+          source = buffer.readUInt8(currentOffset++);
         } catch (error) {
-          const source = '';
           console.log('El error es el siguiente')
           console.error(error);
         }
